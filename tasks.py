b142de1854dbaa1dbbc9fde7c69327b75a107f92
Kai Siren <coilysiren@gmail.com>
2024-11-04 01:19:43 -0800

    create assets

--- tasks.py
+++ tasks.py
@@ -1,1 +1,53 @@
+import os
+import shutil
+import stat
+
 import invoke
+
+
+def handleRemoveReadonly(func, path, _):
+    if not os.access(path, os.W_OK):
+        os.chmod(path, stat.S_IWUSR)
+        func(path)
+    else:
+        raise Exception("could not handle path")
+
+
+def copy_paths(origin_path, target_path):
+    if not os.path.isdir(origin_path):
+        return
+    if os.path.exists(target_path) and os.path.isdir(target_path):
+        print(f"\tRemoving {target_path}")
+        shutil.rmtree(target_path, ignore_errors=False, onerror=handleRemoveReadonly)
+    if os.path.isdir(origin_path):
+        print(f"\tCopying {origin_path} to {target_path}")
+        shutil.copytree(origin_path, target_path)
+
+
+@invoke.task
+def copy_assets(ctx: invoke.Context, branch=""):
+    print("Cleaning out assets folder")
+    if os.path.exists("./eco-server/assets"):
+        shutil.rmtree("./eco-server/assets", ignore_errors=False, onerror=handleRemoveReadonly)
+
+    # get assets from git
+    branch_flag = ""
+    if branch != "":
+        branch_flag = f"-b {branch}"
+    ctx.run(
+        f"git clone --depth 1 {branch_flag} -- git@github.com:coilysiren/eco-mods-assets.git ./eco-server/assets",
+        echo=True,
+    )
+    shutil.rmtree("./eco-server/assets/.git", ignore_errors=False, onerror=handleRemoveReadonly)
+
+    for build in os.listdir("./eco-server/assets/Builds/Mods/UserCode/"):
+        origin_path = os.path.join("./eco-server/assets/Builds/Mods/UserCode", build, "Assets")
+        target_path = os.path.join("./Mods/UserCode", build, "Assets")
+        copy_paths(origin_path, target_path)
+
+
+@invoke.task
+def zip_assets(ctx: invoke.Context, mod):
+    if os.path.exists(f"{mod}.zip"):
+        os.remove(f"{mod}.zip")
+    ctx.run(f"zip -r {mod}.zip ./Mods/UserCode/{mod}")
