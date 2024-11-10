35d8073feb36f86274db2364800eee1f2b1aeeea
Kai Siren <coilysiren@gmail.com>
2024-11-10 00:17:30 -0800

    yaml configs

--- tasks.py
+++ tasks.py
@@ -3,24 +3,13 @@ import shutil
 import stat
 
 import invoke
-import jinja2
+import yaml
 
+import util
 
-AUTOGEN_PATH = os.path.join(
-    "C:\\",
-    "Program Files (x86)",
-    "Steam",
-    "steamapps",
-    "common",
-    "Eco",
-    "Eco_Data",
-    "Server",
-    "Mods",
-    "__core__",
-    "AutoGen",
-)
 
 USERNAME = os.getenv("USERNAME", "")
+USERCODE_PATH = os.path.join("C:\\", "Users", USERNAME, "projects", "eco-mods-public", "Mods", "UserCode")
 
 BUNWULF_CONSTRUCTION_PATH = os.path.join(
     "C:\\", "Users", USERNAME, "projects", "eco-mods-public", "Mods", "UserCode", "BunWulfConstruction", "Recipes"
@@ -29,14 +18,6 @@ BUNWULF_CONSTRUCTION_PATH = os.path.join(
 BUNWULF_EDUCATIONAL_PATH = os.path.join(
     "C:\\", "Users", USERNAME, "projects", "eco-mods-public", "Mods", "UserCode", "BunWulfEducational", "Recipes"
 )
-
-BUNWULF_AGRICULTURAL_PATH = os.path.join(
-    "C:\\", "Users", USERNAME, "projects", "eco-mods-public", "Mods", "UserCode", "BunWulfAgricultural", "Recipes"
-)
-
-
-class TextProcessingException(Exception):
-    pass
 
 
 class RemovalException(Exception):
@@ -98,158 +79,6 @@ def push_asset(ctx: invoke.Context, mod):
     ctx.run(f'ssh -t kai@kai-server "cd {remote_path} && unzip -o {mod}.zip"')
 
 
-############################
-# SPECIALITY MOD FUNCTIONS #
-############################
-
-
-def process_recipes(recipes_changes, target_path):
-    for file, changes in recipes_changes.items():
-        print(f"Reading {file}")
-        with open(os.path.join(AUTOGEN_PATH, file), "r", encoding=",utf-8") as f:
-            recipe_data = f.read()
-
-        for key, configs in changes.items():
-            if configs[0] == "REMOVE-CLASS":
-                recipe_data = remove_class(recipe_data, file, key, configs)
-            elif configs[0] == "REMOVE-CONSTRUCTABLE":
-                recipe_data = remove_constructable(recipe_data, file, key, configs)
-            elif configs[0] == "REMOVE-LINE":
-                recipe_data = remove_line(recipe_data, file, key, configs)
-            else:
-                recipe_data = replace_key(recipe_data, file, key, configs)
-
-        print(f"\tWriting {file}")
-        folder = file.split("\\", maxsplit=1)[0]
-        os.makedirs(os.path.join(target_path, folder), exist_ok=True)
-        with open(os.path.join(target_path, file), "w", encoding="utf-8") as f:
-            f.write(recipe_data)
-
-
-def remove_class(recipe_data, file, key, configs):
-    recipe_lines = recipe_data.split("\n")
-    print(f'\tRemoving "{configs[1]}" from recipe')
-
-    # Step 1: Identify the line where the class starts.
-    class_start_line = 0
-    for line, value in enumerate(recipe_lines):
-        if configs[1] in value:
-            class_start_line = line
-            break
-    if class_start_line == 0:
-        raise TextProcessingException(f"\t\tCouldn't find {configs[1]} in {file}:{key}")
-    print(f'\t\tFound "{configs[1]}" at line {class_start_line}')
-
-    # Step 2: Move class start line upwards if the class has any annotations.
-    for index in range(class_start_line, 0, -1):
-        line = recipe_lines[index].strip()
-        if line.startswith("["):
-            class_start_line = index
-        elif configs[1] in line:
-            continue
-        else:
-            break
-    print(f"\t\tMoved class start line to {class_start_line} due to annotations")
-
-    # Step 3: Count brackets to find the end of the class.
-    bracket_count = 0
-    class_end_index = 0
-    recipe_data = "\n".join(recipe_lines)
-    class_start_index = recipe_data.find(configs[1])
-    backets_started = False
-    for index in range(class_start_index, len(recipe_data)):
-        char = recipe_data[index]
-        if char == "{":
-            bracket_count += 1
-            backets_started = True
-        elif char == "}":
-            bracket_count -= 1
-        elif backets_started and bracket_count == 0:
-            class_end_index = index
-            break
-    if bracket_count != 0 or class_end_index == 0:
-        raise TextProcessingException(f"\t\tCouldn't find end of class in {file}:{key}")
-    class_end_line = recipe_data[:class_end_index].count("\n") + 1
-    print(f"\t\tclass end bracket found at line {class_end_line}")
-
-    # Step 4: Translate the class end index to a line number, remove the lines.
-    print(f"\t\tRemoving lines {class_start_line} to {class_end_line}")
-    recipe_lines = recipe_data.split("\n")
-    del recipe_lines[class_start_line:class_end_line]
-
-    # Step -1: Join the lines back together to get a single string.
-    recipe_data = "\n".join(recipe_lines)
-    return recipe_data
-
-
-def remove_constructable(recipe_data, file, key, configs):
-    recipe_lines = recipe_data.split("\n")
-    print(f"\tRemoving {configs[1]} from recipe")
-
-    # Step 1: Identify the first Tag line.
-    constructable_line = 0
-    for line, value in enumerate(recipe_lines):
-        if configs[1] in value:
-            constructable_line = line
-            break
-    if constructable_line == 0:
-        raise TextProcessingException(f"\t\tCouldn't find {configs[1]} in {file}:{key}")
-
-    # Step 2: This is the end of the file, so remove the lines from constructable_line to the end.
-    print(f"\t\tRemoving lines {constructable_line} to {len(recipe_lines) - 2}")
-    del recipe_lines[constructable_line : len(recipe_lines) - 2]
-
-    # Step -1: Join the lines back together to get a single string.
-    recipe_data = "\n".join(recipe_lines)
-    return recipe_data
-
-
-def remove_line(recipe_data, file, key, configs):
-    recipe_lines = recipe_data.split("\n")
-    print(f"\tRemoving line near {configs[1]} from recipe")
-
-    # Step 1: Identify the line to remove.
-    line_to_remove = 0
-    for line, value in enumerate(recipe_lines):
-        if configs[1] in value:
-            line_to_remove = line
-            break
-    if line_to_remove == 0:
-        raise TextProcessingException(f"\t\tCouldn't find {configs[1]} in {file}:{key}")
-
-    # Step 2: Add offset
-    line_to_remove += configs[2]
-
-    # Step 3: Remove the line.
-    print(f"\t\tRemoving line {line_to_remove}")
-    del recipe_lines[line_to_remove]
-
-    # Step -1: Join the lines back together to get a single string.
-    recipe_data = "\n".join(recipe_lines)
-    return recipe_data
-
-
-def replace_key(recipe_data, file, key, configs):
-    print(f"\tReplacing {key}")
-    if configs[0] not in recipe_data:
-        raise TextProcessingException(f"\t\tCouldn't find {configs[0]} in {file}:{key}")
-    recipe_data = recipe_data.replace(configs[0], configs[1])
-    return recipe_data
-
-
-def template_recipes(recipes_templates, template, target_path):
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader("templates/"))
-    print(f"Rendering templates for {template}")
-
-    for path, values in recipes_templates.items():
-        template = env.get_template(template)
-        content = template.render(**values)
-
-        with open(os.path.join(target_path, path), mode="w", encoding="utf-8") as recipe:
-            recipe.write(content)
-            print(f"\t {path}")
-
-
 #######################
 # SPECIALITY SPECIFIC #
 #######################
@@ -257,37 +86,20 @@ def template_recipes(recipes_templates, template, target_path):
 
 @invoke.task
 def bunwulf_agricultural(_: invoke.Context):
-    recipe_list = [
-        r"Plant\Wheat.cs",
-        r"Plant\Tomatoes.cs",
-        r"Plant\Taro.cs",
-        r"Plant\Beans.cs",
-        r"Plant\Rice.cs",
-        r"Plant\Camas.cs",
-        r"Plant\BoleteMushroom.cs",
-        r"Plant\Pumpkin.cs",
-        r"Plant\PricklyPear.cs",
-        r"Plant\Beets.cs",
-        r"Plant\Pineapple.cs",
-        r"Plant\Papaya.cs",
-        r"Plant\Agave.cs",
-        r"Plant\CriminiMushroom.cs",
-        r"Plant\Huckleberry.cs",
-        r"Plant\Corn.cs",
-        r"Plant\CookeinaMushroom.cs",
-        r"Plant\Fireweed.cs",
-        r"Plant\Fern.cs",
-    ]
+    with open("recipes.yml", "r", encoding="utf-8") as recipes:
+        recipe_data = yaml.safe_load(recipes)["BunWulfAgricultural"]
 
     recipes_templates = {
         template: {
             "plant": template.split("\\")[-1].split(".")[0],
             "species": template.split("\\")[-1].split(".")[0] + "Species",
         }
-        for template in recipe_list
+        for template in recipe_data["recipes"]
     }
 
-    template_recipes(recipes_templates, "plant.template", BUNWULF_AGRICULTURAL_PATH)
+    util.template_recipes(
+        recipes_templates, recipe_data["template"], os.path.join(USERCODE_PATH, "BunWulfAgricultural", "Recipes")
+    )
 
 
 @invoke.task
@@ -501,7 +313,7 @@ def bunwulf_educational(_: invoke.Context):
         },
     }
 
-    process_recipes(recipe_changes, BUNWULF_EDUCATIONAL_PATH)
+    util.process_recipes(recipe_changes, BUNWULF_EDUCATIONAL_PATH)
 
 
 @invoke.task
@@ -584,4 +396,4 @@ def bunwulf_construction(_: invoke.Context):
         },
     }
 
-    process_recipes(recipes_changes, BUNWULF_CONSTRUCTION_PATH)
+    util.process_recipes(recipes_changes, BUNWULF_CONSTRUCTION_PATH)
