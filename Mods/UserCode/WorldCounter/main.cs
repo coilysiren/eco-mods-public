7928d6dd08a667fbc7cc0158e4e9406c6399214d
Kai Siren <coilysiren@gmail.com>
2024-11-20 01:40:10 -0800

    inspect types

--- Mods/UserCode/WorldCounter/main.cs
+++ Mods/UserCode/WorldCounter/main.cs
@@ -5,7 +5,9 @@ namespace WorldCounter
     using Eco.Core.Plugins.Interfaces;
     using Eco.Shared.Localization;
     using Eco.Shared.Logging;
+    using Eco.Shared.Math;
     using Eco.World;
+    using Eco.World.Blocks;
     using Eco.WorldGenerator;
     using Vector3 = System.Numerics.Vector3;
 
@@ -21,7 +23,11 @@ namespace WorldCounter
 
             foreach (PersistentChunk chunk in Chunks)
             {
-                Log.WriteLine(Localizer.DoStr($"Chunk Type: {chunk.GetType().Name}"));
+                Vector3i chunkPosition = chunk.Position;
+                Block block = World.GetBlock(chunkPosition);
+                Type type = block.GetType();
+                Log.WriteLine(Localizer.DoStr($"block postion: {chunkPosition}"));
+                Log.WriteLine(Localizer.DoStr($"block type: {type}"));
             }
         }
 
