6dc6601a390767f4491c2ebd81ef1bfbe1267c8b
kai [they] <coilysiren@gmail.com>
2025-01-09 19:22:49 -0800

    hyper phyto

---
+++ Mods/UserCode/BunWulfBiochemical/Recipes/HyperPhyto.cs
@@ -0,0 +1,122 @@
+namespace Eco.Mods.TechTree
+{
+    using System.Collections.Generic;
+    using Eco.Gameplay.Components;
+    using Eco.Gameplay.Items.Recipes;
+    using Eco.Gameplay.Skills;
+    using Eco.Shared.Localization;
+
+    [RequiresSkill(typeof(BiochemistSkill), 2)]
+    [RequiresModule(typeof(ChemicalLaboratoryObject))]
+    public partial class DryHyperPhyto : RecipeFamily
+    {
+        public DryHyperPhyto()
+        {
+            Recipe recipe = new();
+            recipe.Init(
+                name: "DryHyperPhyto",
+                displayName: Localizer.DoStr("Dry Hyperaccumulator Phytoremediation"),
+                ingredients: new List<IngredientElement>
+                {
+                    new(
+                        typeof(TailingsItem),
+                        1,
+                        staticIngredient: true,
+                    ),
+                    new(
+                        typeof(DirtItem),
+                        1,
+                        staticIngredient: true,
+                    ),
+                    new(
+                        typeof(CompostFertilizerItem),
+                        1,
+                        typeof(BiochemistSkill),
+                        typeof(BiochemistLavishResourcesTalent)
+                    ),
+                    new(
+                        typeof(SunflowerSeedItem),
+                        10,
+                        typeof(BiochemistSkill),
+                        typeof(BiochemistLavishResourcesTalent)
+                    ),
+                },
+                items: new List<CraftingElement> {
+                    new CraftingElement<DirtItem>(2),
+                    new CraftingElement<SunflowerItem>(10)
+                }
+            );
+            Recipes = new List<Recipe> { recipe };
+            LaborInCalories = CreateLaborInCaloriesValue(40, typeof(BiochemistSkill));
+            CraftMinutes = CreateCraftTimeValue(
+                beneficiary: typeof(DryHyperPhyto),
+                start: 30,
+                skillType: typeof(BiochemistSkill),
+                typeof(BiochemistFocusedSpeedTalent),
+                typeof(BiochemistParallelSpeedTalent)
+            );
+            Initialize(
+                displayText: Localizer.DoStr("Dry Hyperaccumulator Phytoremediation"),
+                recipeType: typeof(DryHyperPhyto)
+            );
+            CraftingComponent.AddRecipe(tableType: typeof(FarmersTableObject), recipe: this);
+        }
+    }
+
+    [RequiresSkill(typeof(BiochemistSkill), 2)]
+    [RequiresModule(typeof(ChemicalLaboratoryObject))]
+    public partial class WetHyperPhyto : RecipeFamily
+    {
+        public WetHyperPhyto()
+        {
+            Recipe recipe = new();
+            recipe.Init(
+                name: "WetHyperPhyto",
+                displayName: Localizer.DoStr("Wet Hyperaccumulator Phytoremediation"),
+                ingredients: new List<IngredientElement>
+                {
+                    new(
+                        typeof(TailingsItem),
+                        1,
+                        staticIngredient: true,
+                    ),
+                    new(
+                        typeof(DirtItem),
+                        1,
+                        staticIngredient: true,
+                    ),
+                    new(
+                        typeof(CompostFertilizerItem),
+                        1,
+                        typeof(BiochemistSkill),
+                        typeof(BiochemistLavishResourcesTalent)
+                    ),
+                    new(
+                        typeof(SunflowerSeedItem),
+                        10,
+                        typeof(BiochemistSkill),
+                        typeof(BiochemistLavishResourcesTalent)
+                    ),
+                },
+                items: new List<CraftingElement> {
+                    new CraftingElement<DirtItem>(2),
+                    new CraftingElement<SunflowerItem>(10)
+                }
+            );
+            Recipes = new List<Recipe> { recipe };
+            LaborInCalories = CreateLaborInCaloriesValue(40, typeof(BiochemistSkill));
+            CraftMinutes = CreateCraftTimeValue(
+                beneficiary: typeof(WetHyperPhyto),
+                start: 30,
+                skillType: typeof(BiochemistSkill),
+                typeof(BiochemistFocusedSpeedTalent),
+                typeof(BiochemistParallelSpeedTalent)
+            );
+            Initialize(
+                displayText: Localizer.DoStr("Wet Hyperaccumulator Phytoremediation"),
+                recipeType: typeof(WetHyperPhyto)
+            );
+            CraftingComponent.AddRecipe(tableType: typeof(FarmersTableObject), recipe: this);
+        }
+    }
+}
