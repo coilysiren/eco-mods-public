aa3b7dce7ce73f46bf0638db4f4cabefa68b6125
Kai Siren <coilysiren@gmail.com>
2025-01-12 21:18:00 -0800

    DCC

--- Mods/UserCode/DirectCarbonCapture/DCCItem.cs
+++ Mods/UserCode/DirectCarbonCapture/DCCItem.cs
@@ -1,23 +1,67 @@
 namespace DirectCarbonCapture
 {
+    using Eco.Core.Controller;
+    using Eco.Gameplay.Components;
+    using Eco.Gameplay.DynamicValues;
+    using Eco.Gameplay.Housing.PropertyValues;
     using Eco.Gameplay.Items;
     using Eco.Gameplay.Objects;
     using Eco.Gameplay.Occupancy;
+    using Eco.Gameplay.Systems.NewTooltip;
+    using Eco.Gameplay.Systems.TextLinks;
+    using Eco.Shared.Items;
     using Eco.Shared.Localization;
     using Eco.Shared.Math;
     using Eco.Shared.Serialization;
+    using Eco.Shared.Utils;
 
     [Serialized]
-    [LocDisplayName("Gold Mine")]
+    [Weight(10000)]
+    [LocDisplayName("Direct Carbon Capture Pump Jack")]
     [LocDescription(
-        "For the extraction of gold ore. Must be placed above a gold deposit. Produces waste rock and pollution. Doesn't exhaust the deposit."
+        "Direct carbon Capture removes carbon dioxide (CO2) directly from the ambient air, essentially sucking it out of the atmosphere, and storing it in geological formations."
     )]
-    public partial class DirectCarbonCaptureItem : WorldObjectItem<DirectCarbonCaptureObject>
+    [IconGroup("World Object Minimap")]
+    public partial class DirectCarbonCaptureItem
+        : WorldObjectItem<DirectCarbonCaptureObject>,
+            IPersistentData
     {
         protected override OccupancyContext GetOccupancyContext =>
             new SideAttachedContext(
                 0 | DirectionAxisFlags.Down,
                 WorldObject.GetOccupancyInfo(this.WorldObjectType)
             );
+        public override HomeFurnishingValue HomeValue => homeValue;
+        public static readonly HomeFurnishingValue homeValue = new HomeFurnishingValue()
+        {
+            ObjectName = typeof(DirectCarbonCaptureObject).UILink(),
+            Category = HousingConfig.GetRoomCategory("Industrial"),
+            TypeForRoomLimit = Localizer.DoStr(""),
+        };
+
+        [NewTooltip(CacheAs.SubType, 7)]
+        public static LocString PowerConsumptionTooltip() =>
+            Localizer.Do($"Consumes: {Text.Info(500)}w of {new ElectricPower().Name} power.");
+
+        [
+            Serialized,
+            SyncToView,
+            NewTooltipChildren(CacheAs.Instance, flags: TTFlags.AllowNonControllerTypeForChildren)
+        ]
+        public required object PersistentData { get; set; }
+    }
+
+    [Serialized]
+    [Weight(1)]
+    [LocDisplayName("Carbon Filter")]
+    [LocDescription(
+        "Carbon filters are used to remove carbon dioxide (CO2) from the air. They are used in direct carbon capture systems."
+    )]
+    public partial class CarbonFilterItem : PartItem
+    {
+        public override IDynamicValue SkilledRepairCost => skilledRepairCost;
+        private static IDynamicValue skilledRepairCost = new ConstantValue(1);
+
+        public float ReduceMaxDurabilityByPercent => 0.05f;
     }
 }
--- Mods/UserCode/DirectCarbonCapture/DCCObject.cs
+++ Mods/UserCode/DirectCarbonCapture/DCCObject.cs
@@ -3,44 +3,51 @@ namespace DirectCarbonCapture
     using Eco.Core.Items;
     using Eco.Gameplay.Components;
     using Eco.Gameplay.Components.Auth;
+    using Eco.Gameplay.Housing;
     using Eco.Gameplay.Items;
     using Eco.Gameplay.Objects;
+    using Eco.Gameplay.Occupancy;
     using Eco.Gameplay.Skills;
     using Eco.Mods.TechTree;
     using Eco.Shared.Items;
     using Eco.Shared.Localization;
     using Eco.Shared.Serialization;
-    using Eco.Shared.SharedTypes;
 
     [Serialized]
+    [Tag("Usable")]
     [RequireComponent(typeof(OnOffComponent))]
     [RequireComponent(typeof(PropertyAuthComponent))]
     [RequireComponent(typeof(MinimapComponent))]
     [RequireComponent(typeof(LinkComponent))]
     [RequireComponent(typeof(PartsComponent))]
     [RequireComponent(typeof(AirPollutionComponent))]
+    [RequireComponent(typeof(OccupancyRequirementComponent))]
+    [RequireComponent(typeof(HousingComponent))]
+    [RequireComponent(typeof(PowerConsumptionComponent))]
+    [RequireComponent(typeof(PowerGridComponent))]
     [RepairRequiresSkill(typeof(MechanicsSkill), 1)]
-    [Tag("Usable")]
-    public partial class DirectCarbonCaptureObject : WorldObject
+    [RepairRequiresSkill(typeof(SelfImprovementSkill), 5)]
+    public partial class DirectCarbonCaptureObject : WorldObject, IRepresentsItem
     {
         public override TableTextureMode TableTexture => TableTextureMode.Metal;
-        public override InteractionTargetPriority TargetPriority => InteractionTargetPriority.High;
+        public override LocString DisplayName => Localizer.DoStr("Direct Carbon Capture Pump Jack");
+        public virtual Type RepresentedItemType => typeof(DirectCarbonCaptureItem);
 
         protected override void Initialize()
         {
-            MinimapComponent minimap = this.GetComponent<MinimapComponent>();
-            minimap.SetCategory(Localizer.DoStr("Power"));
+            this.GetComponent<MinimapComponent>().SetCategory(Localizer.DoStr("Power"));
+            this.GetComponent<PowerConsumptionComponent>().Initialize(500);
+            this.GetComponent<PowerGridComponent>().Initialize(10, new ElectricPower());
+            this.GetComponent<HousingComponent>().HomeValue = PumpJackItem.homeValue;
             this.GetComponent<PartsComponent>()
                 .Config(
                     () => LocString.Empty,
                     new PartsComponent.PartInfo[]
                     {
-                        new() { TypeName = nameof(GearboxItem), Quantity = 2 },
+                        new() { TypeName = nameof(CarbonFilterItem), Quantity = 1 },
                     }
                 );
-
-            AirPollutionComponent airPollution = this.GetComponent<AirPollutionComponent>();
-            airPollution.Initialize(-1);
+            this.GetComponent<AirPollutionComponent>().Initialize(-1);
         }
     }
 }
