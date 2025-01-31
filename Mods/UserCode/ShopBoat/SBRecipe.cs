23c8f4109f308ef6b452dd9e52e9b6219365cf0a
kai [they] <coilysiren@gmail.com>
2025-01-30 19:26:36 -0800

    Revert "testing shop boat"
    This reverts commit 04ac553d997230ada3c1bed5f2566225ff35eea0.

--- Mods/UserCode/ShopBoat/SBRecipe.cs
+++ Mods/UserCode/ShopBoat/SBRecipe.cs
@@ -1,43 +1,43 @@
-// namespace ShopBoat
-// {
-//     using System.Collections.Generic;
-//     using Eco.Core.Items;
-//     using Eco.Gameplay.Components;
-//     using Eco.Gameplay.Items.Recipes;
-//     using Eco.Gameplay.Skills;
-//     using Eco.Mods.TechTree;
-//     using Eco.Shared.Localization;
+namespace ShopBoat
+{
+    using System.Collections.Generic;
+    using Eco.Core.Items;
+    using Eco.Gameplay.Components;
+    using Eco.Gameplay.Items.Recipes;
+    using Eco.Gameplay.Skills;
+    using Eco.Mods.TechTree;
+    using Eco.Shared.Localization;
 
-//     [RequiresSkill(typeof(ShipwrightSkill), 3)]
-//     [Ecopedia("Crafted Objects", "Vehicles", subPageName: "Shop Boat Item")]
-//     public partial class ShopBoatRecipe : RecipeFamily
-//     {
-//         public ShopBoatRecipe()
-//         {
-//             Recipe recipe = new();
-//             recipe.Init(
-//                 name: "Shop Boat",
-//                 displayName: Localizer.DoStr("Shop Boat"),
-//                 ingredients: new List<IngredientElement>
-//                 {
-//                     new(typeof(WoodenTransportShipItem), 1, true),
-//                     new(typeof(StoreItem), 1, true),
-//                 },
-//                 items: new List<CraftingElement> { new CraftingElement<ShopBoatItem>() }
-//             );
-//             this.Recipes = new List<Recipe> { recipe };
-//             this.ExperienceOnCraft = 10;
-//             this.LaborInCalories = CreateLaborInCaloriesValue(1000, typeof(ShipwrightSkill));
-//             this.CraftMinutes = CreateCraftTimeValue(
-//                 beneficiary: typeof(ShopBoatRecipe),
-//                 start: 15,
-//                 skillType: typeof(ShipwrightSkill)
-//             );
-//             this.Initialize(
-//                 displayText: Localizer.DoStr("Shop Boat"),
-//                 recipeType: typeof(ShopBoatRecipe)
-//             );
-//             CraftingComponent.AddRecipe(tableType: typeof(MediumShipyardObject), recipe: this);
-//         }
-//     }
-// }
+    [RequiresSkill(typeof(ShipwrightSkill), 3)]
+    [Ecopedia("Crafted Objects", "Vehicles", subPageName: "Shop Boat Item")]
+    public partial class ShopBoatRecipe : RecipeFamily
+    {
+        public ShopBoatRecipe()
+        {
+            Recipe recipe = new();
+            recipe.Init(
+                name: "Shop Boat",
+                displayName: Localizer.DoStr("Shop Boat"),
+                ingredients: new List<IngredientElement>
+                {
+                    new(typeof(WoodenTransportShipItem), 1, true),
+                    new(typeof(StoreItem), 1, true),
+                },
+                items: new List<CraftingElement> { new CraftingElement<ShopBoatItem>() }
+            );
+            this.Recipes = new List<Recipe> { recipe };
+            this.ExperienceOnCraft = 10;
+            this.LaborInCalories = CreateLaborInCaloriesValue(1000, typeof(ShipwrightSkill));
+            this.CraftMinutes = CreateCraftTimeValue(
+                beneficiary: typeof(ShopBoatRecipe),
+                start: 15,
+                skillType: typeof(ShipwrightSkill)
+            );
+            this.Initialize(
+                displayText: Localizer.DoStr("Shop Boat"),
+                recipeType: typeof(ShopBoatRecipe)
+            );
+            CraftingComponent.AddRecipe(tableType: typeof(MediumShipyardObject), recipe: this);
+        }
+    }
+}
