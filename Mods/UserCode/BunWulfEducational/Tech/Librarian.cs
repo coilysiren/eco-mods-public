04e949b9c0672790ac41985d37282287e5a6a22f
Kai Siren <coilysiren@gmail.com>
2025-11-08 15:14:42 -0800

    update desc

--- Mods/UserCode/BunWulfEducational/Tech/Librarian.cs
+++ Mods/UserCode/BunWulfEducational/Tech/Librarian.cs
@@ -15,7 +15,7 @@ namespace BunWulfEducational
     [Serialized]
     [LocDisplayName("Librarian")]
     [LocDescription(
-        "The librarian is an expert in the learning and study of skills. Basic knowledge of the librarian practice allows a person to do all kinds of basic research. Specializing as a librarian unlocks the ability to become a fountain of knowledge, granting the ability to create every skill book and every research paper. Additionally the librarian can create the ink and paper required for modern research."
+        "The librarian is an expert in the learning and study of skills. Basic knowledge of the librarian practice allows a person to do all kinds of basic research. Specializing as a librarian unlocks the ability to become a fountain of knowledge, granting the ability to create every skill book and every research paper."
     )]
     [Ecopedia("Professions", "Scientist", createAsSubPage: true)]
     [RequiresSkill(typeof(ScientistSkill), 0), Tag("Scientist Specialty"), Tier(1)]
